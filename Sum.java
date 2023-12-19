3da95edf16bcc968ec6aa505d1993ed4b0311f71
Tushar D Bidave <tusharbidave@gmail.com>
2023-12-19 22:40:07 +0530

    This is my second commit

--- Sum.java
+++ Sum.java
@@ -1,10 +1,20 @@
-class Sum{
+import java.util.Scanner;
+
+class Sum {
     public static void main(String[] args) {
-        
+
         int a;
         int b;
-        System.out.println(a+b);
+        String data;
+        Scanner sc = new Scanner(System.in);
+        a = sc.nextInt();
+        b = sc.nextInt();
+        data = sc.next();
 
-        
+        System.out.println(a + b);
+        System.out.println(a * b);
+
+        System.out.println(data);
+
     }
 }
